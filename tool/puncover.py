14942d9ef693c086cc22bb164e99368200bb8c19
Heiko Behrens <HeikoBehrens@gmx.de>
2014-09-06 16:20:32 +0200

    first round of HTML templates

--- tool/puncover.py
+++ tool/puncover.py
@@ -5,6 +5,7 @@ from pprint import pprint
 import re
 import subprocess
 import sys
+import jinja2
 
 NAME = "name"
 SIZE = "size"
@@ -14,6 +15,7 @@ LINE = "line"
 ASM = "asm"
 STACK_SIZE = "stack_size"
 STACK_QUALIFIERS = "stack_qualifiers"
+ADDRESS = "address"
 
 def warning(*objs):
     print("WARNING: ", *objs, file=sys.stderr)
@@ -22,6 +24,15 @@ class Collector:
 
     def __init__(self):
         self.symbols = {}
+
+    def qualified_symbol_name(self, symbol):
+        return "%s/%s" % (symbol[BASE_FILE], symbol[NAME]) if symbol.has_key(BASE_FILE) else symbol[NAME]
+
+    def symbol(self, name):
+        for s in self.symbols.values():
+            if self.qualified_symbol_name(s) == name:
+                return s
+        return None
 
     def add_symbol(self, name, address, size=None, file=None, line=None, assembly_lines=None):
         sym = self.symbols.get(address, {})
@@ -38,6 +49,7 @@ class Collector:
             sym[LINE] = line
         if assembly_lines:
             sym[ASM] = assembly_lines
+        sym[ADDRESS] = address
 
         self.symbols[address] = sym
 
@@ -117,47 +129,99 @@ class Collector:
         warning("Couldn't find symbol for %s:%d:%s" % (base_file_name, line, symbol_name))
         return False
 
-
-def get_assembly_lines(dir):
-    proc = subprocess.Popen(['arm-none-eabi-objdump','-dslw', 'pebble-app.elf'], stdout=subprocess.PIPE, cwd=dir)
-    return proc.stdout.readlines()
-
-
-def get_size_lines(dir):
-    proc = subprocess.Popen(['arm-none-eabi-nm','-Sl', 'pebble-app.elf'], stdout=subprocess.PIPE, cwd=dir)
-    return proc.stdout.readlines()
+    def parse_pebble_build_dir(self, dir):
+        def get_assembly_lines(dir):
+            proc = subprocess.Popen(['arm-none-eabi-objdump','-dslw', 'pebble-app.elf'], stdout=subprocess.PIPE, cwd=dir)
+            return proc.stdout.readlines()
 
 
-def gen_find(filepat,top):
-    for path, dirlist, filelist in os.walk(top):
-        for name in fnmatch.filter(filelist,filepat):
-            yield os.path.join(path,name)
+        def get_size_lines(dir):
+            proc = subprocess.Popen(['arm-none-eabi-nm','-Sl', 'pebble-app.elf'], stdout=subprocess.PIPE, cwd=dir)
+            return proc.stdout.readlines()
 
-def gen_open(filenames):
-    for name in filenames:
-        yield open(name)
 
-def gen_cat(sources):
-    for s in sources:
-        for item in s:
-            yield item
+        def gen_find(filepat,top):
+            for path, dirlist, filelist in os.walk(top):
+                for name in fnmatch.filter(filelist,filepat):
+                    yield os.path.join(path,name)
 
-def get_stack_usage_lines(dir):
-    names = gen_find("*.su", os.path.join(dir, "src"))
-    files = gen_open(names)
-    lines = gen_cat(files)
-    return lines
+        def gen_open(filenames):
+            for name in filenames:
+                yield open(name)
+
+        def gen_cat(sources):
+            for s in sources:
+                for item in s:
+                    yield item
+
+        def get_stack_usage_lines(dir):
+            names = gen_find("*.su", os.path.join(dir, "src"))
+            files = gen_open(names)
+            lines = gen_cat(files)
+            return lines
+
+        c.parse_assembly_text("".join(get_assembly_lines(build_dir)))
+        for l in get_size_lines(build_dir):
+            c.parse_size_line(l)
+        for l in get_stack_usage_lines(build_dir):
+            c.parse_stack_usage_line(l)
+
+
+def symbol_url_part(value):
+    return os.path.join(value.get(BASE_FILE, '__builtin'), "%s.html" % value["name"])
+
+
+class HTMLRenderer:
+
+    def __init__(self, collector):
+        self.collector = collector
+        self.template_loader = jinja2.FileSystemLoader(searchpath="templates")
+        self.template_env = jinja2.Environment(loader=self.template_loader)
+        self.template_env.filters["symbol_url"] = symbol_url_part
+        self.template_vars = {
+            "symbols": collector.symbols.values(),
+            "symbols_with_size": list(reversed(sorted([s for s in collector.symbols.values() if s.has_key(SIZE)], key=lambda s: s[SIZE])))
+        }
+
+    def render_overview(self):
+        return self.render_template("overview.html.jinja")
+
+    def render_template(self, template_name):
+        template = self.template_env.get_template(template_name)
+        output = template.render(self.template_vars)
+        return output
+
+    def render_symbol(self, symbol):
+        self.template_vars["symbol"] = symbol
+        return self.render_template("symbol.html.jinja")
+
+    def render_to_path(self, dir):
+        # todo: collect files that exist before and delete them afterwards if they hadn't been regenerated
+
+        def ensure_path(p):
+            if not os.path.exists(p):
+                os.makedirs(p)
+
+        def write(name, content):
+            file_name = os.path.join(dir, name)
+            ensure_path(os.path.dirname(file_name))
+
+            with open(file_name, "w") as f:
+                f.write(content)
+
+        write("index.html", self.render_overview())
+        for s in self.collector.symbols.values():
+            write(symbol_url_part(s), self.render_symbol(s))
+
 
 if __name__ == "__main__":
     build_dir = "/Users/behrens/Documents/projects/pebble/puncover/pebble/build"
 
     c = Collector()
+    c.parse_pebble_build_dir(build_dir)
+    # pprint(c.symbols)
+    r = HTMLRenderer(c)
 
-    c.parse_assembly_text("".join(get_assembly_lines(build_dir)))
-    for l in get_size_lines(build_dir):
-        c.parse_size_line(l)
-    for l in get_stack_usage_lines(build_dir):
-        c.parse_stack_usage_line(l)
-
-    pprint(c.symbols)
-
+    # print(r.render_overview())
+    # print(r.render_symbol(c.symbol("puncover.c/main")))
+    r.render_to_path(os.path.join(build_dir, "puncover"))
