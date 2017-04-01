827c74488b540b5a328b59fcbfaaee88af9723c1
Heiko Behrens <mail@HeikoBehrens.net>
2017-03-31 21:26:46 -0700

    general facelift (#6)
    - more compact visualizations
    - icons
    - percentage bars embedded into tables
    - Kbox to produce screenshots

--- scripts/take_screenshots.sh
+++ scripts/take_screenshots.sh
@@ -5,15 +5,19 @@ mkdir build
 mkdir build/images
 rm build/images/*.png
 
-./runner.py --arm_tools_dir=/Users/behrens/pebble-dev/arm-cs-tools examples/pebble &
-sleep 0.5s
+#./runner.py --arm_tools_dir=/Users/behrens/pebble-dev/arm-cs-tools examples/pebble &
+#sleep 0.5s
 
-PAGERES="pageres 750x500"
-BASE_URL="http://127.0.0.1:5000/path/Users/behrens/Documents/projects/pebble/puncover/pebble"
-$PAGERES --filename='build/images/function' $BASE_URL/src/side_effect.c/use_ptr/
-$PAGERES --filename='build/images/file' $BASE_URL/src/side_effect.c/
-$PAGERES --filename='build/images/folder' $BASE_URL/src/
+#PAGERES=(pageres 750x500 --selector='#inner_content')
+PAGERES=(pageres 750x500)
+
+URL_FILE=http://127.0.0.1:5000/path/Users/behrens/.platformio/packages/framework-arduinoteensy/cores/teensy3/usb_dev.c/
+URL_FOLDER=http://127.0.0.1:5000/path/Users/behrens/Documents/projects/kbox-firmware/lib/KBox/src/
+URL_FUNCTION=http://127.0.0.1:5000/path/Users/behrens/Documents/projects/kbox-firmware/lib/KBox/src/pages/BatteryMonitorPage.cpp/_ZN18BatteryMonitorPage17formatMeasurementEfPKc/
+${PAGERES[@]} --filename='build/images/folder' $URL_FOLDER
+${PAGERES[@]} --filename='build/images/file' $URL_FILE
+${PAGERES[@]} --filename='build/images/function' $URL_FUNCTION
 
 convert build/images/function.png build/images/file.png build/images/folder.png +append images/overview.png
 
-kill $(jobs -p)
+#kill $(jobs -p)
