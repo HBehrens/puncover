d4a38ce92ffc7ae32bfaa6806803cc7bc0a76528
Tyler Hoffman <tyler@memfault.com>
2020-05-05 02:23:51 -0700

    Setup Tox and Travis Build Matrix
    Summary:
    The way I've done build matrices in the past is to use Tox to handle all
    the tedious work of setting up virtul aenvironments and running the
    tests.
    
    It also looks like there's a nice plugin,
    [tox-travis](https://github.com/tox-dev/tox-travis), which takes in the
    evironment definsed to run the appropriate tests.
    
    Unfortunately, when this was run against Pythonn 3.5, the tests failed,
    so that's a later PR.
    
    Test Plan:
    - Run `tox` locally and confirm the tests pass on py36, py37, and py38

--- setup.py
+++ setup.py
@@ -4,15 +4,13 @@ import os
 from setuptools import setup, find_packages, Command
 
 __version__ = None  # Overwritten by executing version.py.
-with open('puncover/version.py') as f:
-    exec (f.read())
-
-with open('requirements.txt') as f:
-    requires = f.readlines()
+with open("puncover/version.py") as f:
+    exec(f.read())
 
 
 class CleanCommand(Command):
     """Custom clean command to tidy up the project root."""
+
     # http://stackoverflow.com/a/3780822/196350
     user_options = []
 
@@ -23,24 +21,23 @@ class CleanCommand(Command):
         pass
 
     def run(self):
-        os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
+        os.system("rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info")
 
 
-setup(name='puncover',
-      version=__version__,
-      description='Analyses C/C++ build output for code size, static variables, and stack usage.',
-      long_description=open('README.rst').read(),
-      url='https://github.com/hbehrens/puncover',
-      download_url='https://github.com/hbehrens/puncover/tarball/%s' % __version__,
-      author='Heiko Behrens',
-      license='MIT',
-      packages=find_packages(exclude=['tests', 'tests.*']),
-      include_package_data=True,
-      zip_safe=False,
-      entry_points={'console_scripts': ['puncover = puncover.puncover:main']},
-      install_requires=requires,
-      test_suite='nose.collector',
-      cmdclass={
-          'clean': CleanCommand,
-      }
-      )
+setup(
+    name="puncover",
+    version=__version__,
+    description="Analyses C/C++ build output for code size, static variables, and stack usage.",
+    long_description=open("README.rst").read(),
+    url="https://github.com/hbehrens/puncover",
+    download_url="https://github.com/hbehrens/puncover/tarball/%s" % __version__,
+    author="Heiko Behrens",
+    license="MIT",
+    packages=find_packages(exclude=["tests", "tests.*"]),
+    include_package_data=True,
+    zip_safe=False,
+    entry_points={"console_scripts": ["puncover = puncover.puncover:main"]},
+    install_requires=["Flask==0.10.1", "mock==1.3.0", "codecov==2.0.5", "nose-cov==1.6"],
+    test_suite="nose.collector",
+    cmdclass={"clean": CleanCommand,},
+)
