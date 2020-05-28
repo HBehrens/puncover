8ca6c73779f46eea63e332f1e7155117dd5a38a9
Tyler Hoffman <tyler@memfault.com>
2020-05-27 22:43:01 -0700

    Prep for PyPi upload
    Summary:
    - Set the version to 0.1.0
    - Add proper PyPi classifiers
    - Require the correct Python versions until the Python 3.5 bug is fixed
    
    Test Plan:
    - I uploaded the package to test.pypi.org under a different name and I
    have confirm that I can install the package and use it under a fresh Conda
    environment
    - Run tox locally to confirm 3.6-3.8 works

--- setup.py
+++ setup.py
@@ -33,10 +33,22 @@ setup(
     version=__version__,
     description="Analyses C/C++ build output for code size, static variables, and stack usage.",
     long_description=open("README.rst").read(),
+    long_description_content_type="text/x-rst",
     url="https://github.com/hbehrens/puncover",
     download_url="https://github.com/hbehrens/puncover/tarball/%s" % __version__,
     author="Heiko Behrens",
     license="MIT",
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Developers",
+        "Natural Language :: English",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.6",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+    ],
     packages=find_packages(exclude=["tests", "tests.*"]),
     include_package_data=True,
     zip_safe=False,
@@ -45,4 +57,7 @@ setup(
     tests_require=tests_require,
     test_suite="nose.collector",
     cmdclass={"clean": CleanCommand,},
+    # TODO: https://github.com/HBehrens/puncover/issues/36
+    #  Fix Python 3.5
+    python_requires=">=3.6",
 )
