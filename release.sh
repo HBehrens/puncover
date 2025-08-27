883ac2eb242c9500de827fa238414548ba0a0c8c
Noah Pendleton <2538614+noahp@users.noreply.github.com>
2025-08-27 12:57:02 -0400

    Move to pyproject.toml

--- release.sh
+++ release.sh
@@ -34,26 +34,22 @@ if git rev-parse -q --verify "refs/tags/${PUNCOVER_VERSION}"; then
     exit 1
 fi
 
-PUNCOVER_VERSION_COMMA_DELIMITED=$(echo ${PUNCOVER_VERSION} | sed 's/\./, /g')
-
-if grep -q "${PUNCOVER_VERSION_COMMA_DELIMITED}" puncover/version.py; then
-    echo "Version ${PUNCOVER_VERSION} already set in puncover/version.py"
-    exit 1
-fi
-
-# little sed magic to update the version in the code
-sed -i -r 's/(.*__version_info__ = )\(.*\)/\1\('"${PUNCOVER_VERSION_COMMA_DELIMITED}"'\)/g' puncover/version.py
+uv version ${PUNCOVER_VERSION}
 git add . && git commit -m "Bump version to ${PUNCOVER_VERSION}"
 git tag -a {-m=,}${PUNCOVER_VERSION}
 rm -rf dist
 uv build
 
-read -p "Ready to push to GitHub and publish to PyPi? " -n 1 -r
-echo    # move to a new line
-if [[ $REPLY =~ ^[Yy]$ ]]
-then
-    git push && git push --tags
-    uv publish --token=${PYPI_TOKEN} dist/*
-    gh release create --generate-notes ${PUNCOVER_VERSION}
-    gh release upload ${PUNCOVER_VERSION} dist/*
-fi
+while true; do
+    read -p "Ready to push to GitHub and publish to PyPi? (y/n): " yn
+    case $yn in
+        [Yy]* ) echo "Proceeding..."; break;;
+        [Nn]* ) echo "Exiting..."; exit;;
+        * ) echo "Invalid response. Please answer y or n.";;
+    esac
+done
+
+git push && git push --tags
+uv publish --token=${PYPI_TOKEN} dist/*
+gh release create --generate-notes ${PUNCOVER_VERSION}
+gh release upload ${PUNCOVER_VERSION} dist/*
